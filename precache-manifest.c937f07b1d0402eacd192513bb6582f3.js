e289c8384c0b394b32a371277f1e7ce588aa8c66
Alain Afonso <alainpedro123@gmail.com>
2019-07-17 23:38:07 +0200

    Updates

---
+++ precache-manifest.c937f07b1d0402eacd192513bb6582f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65dfaf60bed1fae9dce50b9945a2085a",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "73c6fc162e37f18021dd",
+    "url": "/face-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "379a47895bdbaa8c4ec8",
+    "url": "/face-recognition/static/css/main.4babcbd5.chunk.css"
+  },
+  {
+    "revision": "73c6fc162e37f18021dd",
+    "url": "/face-recognition/static/js/2.97e7b1a1.chunk.js"
+  },
+  {
+    "revision": "379a47895bdbaa8c4ec8",
+    "url": "/face-recognition/static/js/main.72711ce6.chunk.js"
+  },
+  {
+    "revision": "2873375fc50fb304d268",
+    "url": "/face-recognition/static/js/runtime~main.4924a73e.js"
+  }
+]);
